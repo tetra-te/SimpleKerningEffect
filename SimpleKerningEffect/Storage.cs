fa8e182d87780c23f7d3c835f97cbf0aab696406
tetra_te <node4853@gmail.com>
2025-04-13 19:07:28 +0900

    Textsのgetterメソッドを追加

--- SimpleKerningEffect/Storage.cs
+++ SimpleKerningEffect/Storage.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Concurrent;
+using YukkuriMovieMaker.Player.Video;
 
 namespace SimpleKerningEffect
 {
     public static class Storage
     {
         public static ConcurrentDictionary<(Guid, int), string> Texts = new();
+
+        public static string GetText(EffectDescription effectDescription)
+        {
+            return Texts.GetValueOrDefault((effectDescription.SceneId, effectDescription.Layer), "");
+        }
     }
 }
