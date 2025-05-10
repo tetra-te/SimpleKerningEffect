c493d00495d8347732b462f6de2acf0bc2a031b6
tetra_te <node4853@gmail.com>
2025-05-10 21:50:58 +0900

    ボイスアイテムの字幕で簡易カーニングの一部の機能が利用できない問題を修正

--- SimpleKerningEffect/Patch/Main.cs
+++ SimpleKerningEffect/Patch/Main.cs
@@ -12,9 +12,12 @@ namespace SimpleKerningEffect.Patch
             var harmony = new Harmony("SimpleKerningEffect.TextInfoAcquisitionPatch");
 
             var textOriginal = AccessTools.Method("YukkuriMovieMaker.Player.Video.Items.TextSource:UpdateResource");
+            var jimakuOriginal = AccessTools.Method("YukkuriMovieMaker.Player.Video.Items.JimakuSource:UpdateResource");
             var textPostfix = typeof(PatchText).GetMethod(nameof(PatchText.Postfix));
+            var jimakuPostfix = typeof(PatchJimaku).GetMethod(nameof(PatchJimaku.Postfix));
 
             harmony.Patch(textOriginal, new HarmonyMethod(textPostfix));
+            harmony.Patch(jimakuOriginal, new HarmonyMethod(jimakuPostfix));
         }
     }
 }
---
+++ SimpleKerningEffect/Patch/PatchJimaku.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using YukkuriMovieMaker.Player.Video;
+
+namespace SimpleKerningEffect.Patch
+{
+    public class PatchJimaku
+    {     
+        public static void Postfix(TimelineItemSourceDescription desc, object __instance)
+        {
+            var jimakuSourceType = __instance.GetType();
+            var itemField = AccessTools.Field(jimakuSourceType, "item");
+            var item = itemField.GetValue(__instance);
+
+            var voiceItemType = item.GetType();
+            var serifProp = AccessTools.Property(voiceItemType, "Serif");
+            var serif = serifProp.GetValue(item);
+
+            Storage.Texts[(desc.SceneId, desc.Layer)] = (string)serif;
+        }
+    }
+}
